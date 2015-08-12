5a83d196456f623e360a62952fefdb2ffc4266e1
toming <tomingtoming@gmail.com>
2015-08-12 22:25:39 +0900

    Simplify class type signature

--- src/main/scala/susuru/twitter/TwitterSusuru.scala
+++ src/main/scala/susuru/twitter/TwitterSusuru.scala
@@ -4,8 +4,6 @@ import susuru.core.resource.Susuru
 import susuru.core.resource.Id
 import twitter4j.{TwitterResponse, Twitter}
 
-class TwitterSusuru(source: () => Map[Id, Twitter])
-  extends Susuru[Twitter, TwitterResponse](
-    source, r => (r.getRateLimitStatus.getRemaining, r.getRateLimitStatus.getResetTimeInSeconds * 1000)
-  ) {
-}
+class TwitterSusuru(source: () => Map[Id, Twitter]) extends Susuru[Twitter, TwitterResponse](
+  source, r => (r.getRateLimitStatus.getRemaining, r.getRateLimitStatus.getResetTimeInSeconds * 1000)
+)

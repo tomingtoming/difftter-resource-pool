3b489127d9d1421bf37ba6baefa3c7f1f56e5c88
toming <tomingtoming@gmail.com>
2015-10-11 07:34:38 +0900

    Pool.release should be applied by resource status

--- src/main/scala/susuru/core/Pool.scala
+++ src/main/scala/susuru/core/Pool.scala
@@ -1,7 +1,10 @@
 package susuru.core
 
 trait Pool[R] {
-   def lease(): R
-   def lease(id: Long): R
-   def release(): Unit
- }
+
+  def lease(): R
+
+  def lease(id: Long): R
+
+  def release(id: Long, count: Int, until: Long, resource: R): Unit
+}
